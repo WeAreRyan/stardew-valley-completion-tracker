df93cbd1eace16d72d976d6d24bec38fb136bd5a
rdokamoto@gmail.com <ryan.okamoto@Ryans-MacBook-Pro.local>
2022-06-27 15:59:03 -0700

    finishes oauth and adds save data model

--- models/save.js
+++ models/save.js
@@ -2,6 +2,45 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const saveSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User', required: true
+    },
+    saveName: {
+        type: String, required: true
+    },
+    community: {
+        type: Boolean
+    }, 
+    items: {
+        type: Boolean
+    }, 
+    buildings: {
+        type: Boolean
+    }, 
+    monsers: {
+        type: Boolean
+    },
+    friendship: {
+        type: Boolean
+    },
+    skills: {
+        type: Boolean
+    }, 
+    stardrops: {
+        type: Boolean
+    },
+    Recipes: {
+        type: Boolean
+    }, 
+    Crafting: {
+        type: Boolean, 
+    }, 
+    Fish: {
+        type: Boolean
+    }, 
+    Wallnuts: {
+        type: Boolean
+    }
     
 })
 
--- routes/saves.js
+++ routes/saves.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var savesCtrl = require('../controllers/saves');
+const isLoggedIn = require('../config/auth');
+
 
 router.get('/', savesCtrl.index);
 
